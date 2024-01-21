3b59e90cfc737ab9ff26ee9d8cfe72df30345826
Lucas Barros <lucasgleyfson59@gmail.com>
2024-01-21 19:00:34 -0300

    atualização 2

--- exercício_jquery/main.js
+++ exercício_jquery/main.js
@@ -1,3 +1,21 @@
-$(document).ready(function() {
-    alert("olá jquery")
-} )
+
+$(document).ready(function () {
+    // Função para adicionar tarefa à lista
+    $('form').submit(function (event) {
+        event.preventDefault();
+
+        const NomeTarefa = $('#NomeTarefa').val();
+
+        $('#Lista').append("<li>" + NomeTarefa + "</li>");
+
+        $('#NomeTarefa').val("");
+    });
+
+    // Função para aplicar o efeito de linha quando um item da lista é clicado
+    $('#Lista').on('click', 'li', function () {
+        $(this).toggleClass('linha-through');
+    });
+});
+
+
+
