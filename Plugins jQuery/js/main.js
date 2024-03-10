99d3407c3c7897d28d50f3e2e123e5bd4edb4efc
Lucas Barros <lucasgleyfson59@gmail.com>
2024-03-10 15:18:05 -0300

    Commit

--- Plugins jQuery/js/main.js
+++ Plugins jQuery/js/main.js
@@ -3,3 +3,40 @@ $('#telefone').mask('(00) 00000-0000');
 $('#cep').mask('00000-000');
 
 $('#cpf').mask('000.000.000-00');
+
+
+$('form').validate({
+    rules: {
+        nome: {
+            required: true
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        telefone: {
+            required: true
+        },
+        cpf: { required: true },
+        endereco: { required: true },
+        cep: { required: true },
+    },
+    messages: {
+        nome: "Insira seu nome completo",
+        email: "Insira um email valido",
+        telefone: "Insira um numero de celular",
+        cpf: "Adicione seu CPF",
+        endereco: "Insira seu endenço completo",
+        cep: "Insira o CEP",
+    },
+    submitHandler: function (form) {
+        alert(`Cadastro concluído com sucesso!`);
+
+        nome.value = "";
+        email.value = "";
+        telefone.value = "";
+        cpf.value = "";
+        endereco.value = "";
+        cep.value = "";
+    },
+})
