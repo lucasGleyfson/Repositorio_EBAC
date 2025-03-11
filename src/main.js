029eb31011d125c3607df3fc854bbe35aafb4e72
lucas barros <lucasgleyfson59@gmail.com>
2025-03-11 14:11:03 -0300

    Primeiro commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 + num2;
+}
+function saudacao(nome) {
+    return `olá ${nome} `;
+}
+console.log(multiplicacao(10, 20));
+console.log(saudacao('lucas'));
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function multiplicacao (num1: number, num2:number) {
+    return num1 + num2;
+}
+function saudacao (nome: string) {
+    return `olá ${nome} `
+}
+
+
+console.log(multiplicacao(10,20));
+console.log(saudacao('lucas'));
